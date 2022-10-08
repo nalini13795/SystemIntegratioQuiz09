7d9d9f986d846b3fdeba9cdf38f91ef9fd7eb3c9
nalini_katiyar <nalinikatiyar@gmail.com>
2022-10-08 14:04:48 -0400

    my fuc changed

--- my-function.js
+++ my-function.js
@@ -1,7 +1,11 @@
 exports.handler = async (event) => {
     // TODO implement
     var return_value = JSON.parse(event.body)
-    var return_string = `Nalini Katiyar says ${return_value.keyword}`
+    if(return_value.keyword){
+        var return_string = `Nalini Katiyar says ${return_value.keyword}`
+    }else{
+        var return_string = `Nalini Katiyar says `
+    }
     const response = {
         statusCode: 200,
         body: return_string
